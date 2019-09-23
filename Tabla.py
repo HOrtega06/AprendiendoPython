cf0ea060a44b83df91ce72fd45f6ba89b2375876
HiramLTI <55333283+HiramLTI@users.noreply.github.com>
2019-09-23 11:56:13 -0500

    Update Tabla.py

--- Tabla.py
+++ Tabla.py
@@ -1,12 +1,10 @@
 numero=input("Dame un numero del 1 al 9: ")
 numero=int(numero)
 
-#for ejecuta un bloque de codigo un numero determinado
-#de veces cuando se pide que recorra un rango de
-#valores. El segundo parametro de range no se incluye
-#en la serie de valores. Aqui seria del 1 al 10 (11 no cuenta, es limite del rango)
+#for es usado para ciclos y se usa con range para repetir un proceso
+#el numero de veces que se le asigne al mismo (range, sin contar el limite)
 for i in range(1,11):
-    # i va variando a cada iteracion
+    # i cambia al iterar repetidas veces
     salida="{} x {} = {}"
     print(salida.format(numero,i,numero*i))
 
