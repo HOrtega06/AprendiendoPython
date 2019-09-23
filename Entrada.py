532154771594826940344ddf831f72c46d61ab5c
HiramLTI <55333283+HiramLTI@users.noreply.github.com>
2019-09-23 11:49:04 -0500

    Update Entrada.py

--- Entrada.py
+++ Entrada.py
@@ -1,16 +1,9 @@
 entrada=input()
 print(type(entrada))
-#La variable booleana contiene el resultado de verificar si lo
-#capturado es digito, y si no se encuentra un punto en lo capturado,
-#lo que indicaria que trata de un numero con decimales, es decir un float.
-#Si find retorna -1 quiere decir que lo buscado, en este caso es True,
-#igual a que lo capturado es entero
 esEntero=(entrada.isdigit() and entrada.find(".")==-1)
 if (esEntero):
-    #Se ejecutara si la condicion es verdadera(True)
     print("Dato entero. ¡Muy bien!.")
 else:
-    #Se ejecutara si la condicion es falsa(False)
     print("Dato no es entero. Intentar nuevamente.")
 
 #Elaboramos un programa que declare una variable que reciba un valor;
