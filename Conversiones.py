a14634d42c16be9e2d2b4e0ee7995cf291996d26
HiramLTI <55333283+HiramLTI@users.noreply.github.com>
2019-09-23 11:43:11 -0500

    Update Conversiones.py

--- Conversiones.py
+++ Conversiones.py
@@ -1,18 +1,15 @@
-#Declaramos variable str, con cadena de numeros.
+#Declaramos una cadena
 numero = "1234"
-#Se MUESTRA el tipo de variable ,
 # type = da un dato type, no un str(<class 'str'>).
 print(type(numero))
 #Convertimos la cadena a int.
 numero=int(numero)
-#Se MUESTRA como el tipo ha cambiado,
-#aunque se usa la misma variable (<class 'int'>).
+#Cambiamos el tipo de la variable auqnue esta
+#siga siendo la misma(<class 'int'>).
 print(type(numero))
-#Se declara un str con meta sustitucion
-#(posiciones donde iran valores usando format).
+#Usamos meta sustitucion
+#format se usa para dar posiscion a valores
 salida="El numero utilizado es {}"
-#Se MUESTRA el resultado. La meta sustitucion hara que donde
-#donde esta {} se coloque el valor de la variable numero, (El numero utilizado es 1234).
 print(salida.format(numero))
 
 #Declaramos una variable de tipo str con un valor de "1234", y mostramos
