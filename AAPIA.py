b97c9ae65dc9ab86aed40746df79cac7117071d4
HiramLTI <55333283+HiramLTI@users.noreply.github.com>
2019-11-13 12:01:47 -0600

    Add files via upload

---
+++ AAPIA.py
@@ -0,0 +1,54 @@
+import json
+class Bicicletas():
+    def __init__(self,Color):
+       self.Altura=90
+       self.Largo=150
+       self.Llantas=2
+       self.Color=Color
+       self.Avanza=False
+#Comportamientos
+    def Avanzar(self):
+        self.Avanza=True
+
+    def Informe(self):
+        print("La bicicleta tiene",self.Altura,"En centrimentros")
+
+bicicleta1=Bicicletas("Negro")
+bicicleta2=Bicicletas("Amarillo")
+bicicleta3=Bicicletas("Azul")
+
+lista_bici=(bicicleta1,bicicleta2,bicicleta3)
+print(lista_bici)
+
+json=json.dumps(lista_bici,default=lambda o: o.__dict__, indent=5)
+print(json)
+
+print(isinstance(bicicleta1, Bicicletas))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
